790e1f3f5c015aa1bce8cdceb15b6ee69859dd69
Jenny Robinson <jennykmorgan@gmail.com>
2023-02-15 21:15:43 +0000

    use spcay for similarity matching

--- watch_next.py
+++ watch_next.py
@@ -4,11 +4,23 @@ nlp = spacy.load('en_core_web_md')
 
 tokens = nlp('car bus man wheel ')
 
-# comment about someting
 file_in = open("movies.txt", "r")
+movie_data = file_in.readlines()
 
-data = file_in.readlines()
+def get_similar_film(description):
+    description_token = nlp(description)
+    highest_similarity = 0
+    best_match_index = 0
+    for pos, movie_line in enumerate(movie_data, 0):
+        split_data = movie_line.strip("\n").split(" :")
+        token = nlp(split_data[1])
+        token_similarity = token.similarity(description_token)
+        if token_similarity > highest_similarity:
+            highest_similarity = token_similarity
+            best_match_index = pos
 
-# loop over each line of data
-for pos, line in enumerate(data, 0):
-    print(line)
+    movie = movie_data[best_match_index].strip("\n").split(" :")
+    print(f"Best match: {movie[0]}")
+
+description = "Will he save their world or destroy it? When the Hulk becomes too dangerous for the Earth, the Illuminati trick Hulk into a shuttle and launch him into space to a planet where the Hulk can live in peace. Unfortunately, Hulk land on the planet Sakaar where he is sold into slavery and trained as a gladiator."
+get_similar_film(description)
